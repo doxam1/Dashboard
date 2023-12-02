9f21c57ff5427d5ff8bf969f09f10a67bca75de4
Doxam1 <143098976+doxam1@users.noreply.github.com>
2023-12-02 13:25:13 +0200

    change header, sidebar position=sticky.
    canceled script.
    canceled media queries.

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const LogoSideBar = document.querySelector('.logoSideBar');
-const sideBar = document.querySelector('.sideBar');
-const middleSideBar = document.querySelector('.middleSideBar');
-const bottomSideBar = document.querySelector('.bottomSideBar');
+// const LogoSideBar = document.querySelector('.logoSideBar');
+// const sideBar = document.querySelector('.sideBar');
+// const middleSideBar = document.querySelector('.middleSideBar');
+// const bottomSideBar = document.querySelector('.bottomSideBar');
 
 
 
-window.onresize = ()=> {
-    if (window.innerWidth > 500) {
-        sideBar.style.position = 'static';
-        middleSideBar.style.display = 'grid';
-        bottomSideBar.style.display = 'grid';
-    } else {
-        sideBar.style.position = 'absolute';
-        middleSideBar.style.display = 'none';
-        bottomSideBar.style.display = 'none';
-    }
-}
-LogoSideBar.onclick = () => {
-    if (window.innerWidth < 500 ) {
-    if (sideBar.style.position == 'absolute') {
-        sideBar.style.position = 'static';
-        middleSideBar.style.display = 'grid';
-        bottomSideBar.style.display = 'grid'
-    } else {
-        sideBar.style.position = 'absolute';
-        middleSideBar.style.display = 'none';
-        bottomSideBar.style.display = 'none';
-        };   
-}}
+// window.onresize = ()=> {
+//     if (window.innerWidth > 500) {
+//         sideBar.style.position = 'static';
+//         middleSideBar.style.display = 'grid';
+//         bottomSideBar.style.display = 'grid';
+//     } else {
+//         sideBar.style.position = 'absolute';
+//         middleSideBar.style.display = 'none';
+//         bottomSideBar.style.display = 'none';
+//     }
+// }
+// LogoSideBar.onclick = () => {
+//     if (window.innerWidth < 500 ) {
+//     if (sideBar.style.position == 'absolute') {
+//         sideBar.style.position = 'static';
+//         middleSideBar.style.display = 'grid';
+//         bottomSideBar.style.display = 'grid'
+//     } else {
+//         sideBar.style.position = 'absolute';
+//         middleSideBar.style.display = 'none';
+//         bottomSideBar.style.display = 'none';
+//         };   
+// }}
 
